2aadf3fb82448e3996f1c90bbc9fe89e08ff6a0d
oleg-agapov <oleg-agapov@users.noreply.github.com>
2023-06-26 16:04:39 +0000

    week 2 solution

---
+++ tests/test_quote_disp.py
@@ -0,0 +1,23 @@
+import pytest
+from quote_disp.app import app
+
+
+@pytest.fixture
+def test_client():
+    with app.test_client() as testing_client:
+        with app.app_context():
+            yield testing_client
+
+
+def test_home_page(test_client):
+    res = test_client.get("/")
+    assert res.status_code == 200
+
+def test_health_page(test_client):
+    res = test_client.get("/health")
+    assert res.status_code == 200
+    assert "healthy" in str(res.data)
+
+# def test_quote_page(test_client):
+#     res = test_client.get("/get_quote")
+#     assert res.status_code == 200
